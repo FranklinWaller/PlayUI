13325e6053921d0fa4a7aad14abbd7f99bd91396
Franklin Waller <franklin.dijks@gmail.com>
2018-04-19 18:38:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+PlayUI
+=====
+
+Building a UI framework for building applications on the <canvas> element.
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const exclude = '/node_modules';
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    entry: {
+        index: ['./index.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, './build/'),
+        libraryTarget: 'umd',
+        filename: '[name].js',
+        publicPath: '/build/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                loaders: [
+                    {
+                        loader: 'style-loader?sourceMap',
+                    }, {
+                        loader: 'css-loader',
+                        options: {
+                            localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                        },
+                    }, {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+};
